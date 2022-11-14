ac57f868f90e1fab683a76a83dc322797e18c888
Mochamad Haikal <mhaikal1006@gmail.com>
2022-11-14 17:03:21 +0700

    error api

--- src/services/blog.ts
+++ src/services/blog.ts
@@ -1,23 +1,24 @@
 import axios from "axios";
 
-const URL = 'https://newsapi.org/v2/everything?apiKey=53a9a9f01bb546319132d90da36ef5bc'
+const URL =
+  "https://newsapi.org/v2/everything?apiKey=f957b14dc5334638b631aa603d6e9f71";
 
 export const getBlog = async () => {
   const response = await axios.get(URL, {
     params: {
-      sortBy: 'publishedAt',
-      from: '2022-06-20',
-      q: 'color'
-    }
-  })
-  
+      sortBy: "publishedAt",
+      from: "2022-10-14",
+      q: "color",
+    },
+  });
+
   if (!response.status) {
     throw new Error("Fetching Error");
   }
 
-  const data = await response.data?.articles?.splice(0,30)
+  const data = await response.data?.articles?.splice(0, 30);
 
-  localStorage.setItem('news', JSON.stringify(data || []))
-  
-  return data || []
-}
+  localStorage.setItem("news", JSON.stringify(data || []));
+
+  return data || [];
+};
