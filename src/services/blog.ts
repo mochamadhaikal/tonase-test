e0571da864398d6fae69c257e87b11bc16248576
Mochamad Haikal <mhaikal1006@gmail.com>
2022-11-30 13:14:03 +0700

    ganti api

--- src/services/blog.ts
+++ src/services/blog.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 const URL =
-  "https://newsapi.org/v2/everything?apiKey=f957b14dc5334638b631aa603d6e9f71";
+  "https://newsapi.org/v2/top-headlines?apiKey=f957b14dc5334638b631aa603d6e9f71";
 
 export const getBlog = async () => {
   const response = await axios.get(URL, {
     params: {
-      sortBy: "publishedAt",
-      from: "2022-10-14",
-      q: "color",
+      country:"us",
+      category:"business",
     },
   });
 
